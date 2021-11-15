35d5986209d2402531d9472c0c20d6f8a35a56b7
Joe Klemke <jklemke@grox.com>
2021-11-15 18:45:13 +0000

    renamed schema to signature, model to grammar

---
+++ js/prj/signature.js
@@ -0,0 +1,364 @@
+// Copyright 2021,  Joe Klemke, Grox LLC
+// Distributed under GNU LESSER GENERAL PUBLIC LICENSE, http://www.gnu.org/licenses/lgpl.txt
+
+// top level grox namespace
+var grox = grox || {};
+
+// Signature holds the basic structure of namespaces, signifiers, axioms 
+grox.Signature = 
+(
+	// anonymous IIFE function that is called once after the code is parsed, to define the static attributes and methods, and to return the constructor function
+	function() 
+	{
+		// private static attribute (defined once and shared by all Signature Attributatives)
+
+		// the actual constructor function which gets invoked by new Signature()
+		return function(model) 
+		{
+			// private attributes, unique to each Signature instance
+			let _namespaces = {};
+			let _axioms = [];
+			let _signifiers = {};
+			let _thisSignature = this;
+
+			// private methods, unique to each Signature instance, with access to private attributes and methods
+			let _isTypeOfSignifier = function(value)
+			{
+        if(
+          value == undefined ||
+          typeof value != "object" ||
+          value.getQName == undefined || 
+          value.notifyOfParticipationAsNominative == undefined || 
+          value.notifyOfParticipationAsCopulative == undefined || 
+          value.notifyOfParticipationAsAttributative == undefined 
+          )  
+        {
+          return false;
+        } 
+        else  
+        {
+          return true;
+        }
+      }
+
+			// _Signifier is an IIFE constructor function which is private to Signature
+			let _Signifier = 
+			(
+				function () 
+				{
+					return function(QName,prefLabel)
+					{
+						// private to each _Signifier instance
+						let _QName;
+						let _prefLabel;
+						let _axiomsWithThisAsNominative = [];
+						let _axiomsWithThisAsCopulative = [];
+						let _axiomsWithThisAsAttributative = [];
+			
+						this.notifyOfParticipationAsNominative = function(axiom) 
+						{
+							_axiomsWithThisAsNominative.push(axiom);
+							if (axiom.getCopulativeLabel() != undefined)
+							{
+								this[axiom.getCopulativeLabel()] = axiom.getAttributative();
+							}
+						};
+			
+						this.notifyOfParticipationAsCopulative = function(axiom) 
+						{
+							_axiomsWithThisAsCopulative.push(axiom);
+						};
+			
+						this.notifyOfParticipationAsAttributative = function(axiom) 
+						{
+							_axiomsWithThisAsAttributative.push(axiom);
+						};
+			
+						this.getQName = function() 
+						{
+							return _QName;
+						};
+			
+						this.getPrefLabel = function() 
+						{
+							return _prefLabel;
+						};
+
+						this.setPrefLabel = function(prefLabel) 
+						{
+							_prefLabel =prefLabel;
+							return _prefLabel;
+						};
+
+						// TODO: these get statements are the beginning of a SELECT API
+						// there may be multiple theorems for a single axiom/triple
+						this.getAxiomsWithThisAsNominative = function() 
+						{
+							return _axiomsWithThisAsNominative;
+						};
+			
+						this.getAxiomsWithThisAsCopulative = function() 
+						{
+							return _axiomsWithThisAsCopulative;
+						};
+						
+						this.getAxiomsWithThisAsAttributative = function() 
+						{
+							return _axiomsWithThisAsAttributative;
+						};
+			
+						// _Signifier constructor code
+						if (!QName) {throw new Error("Invalid QName for new signifier, " + QName + ".");}
+						if (typeof QName != "string") {throw new Error("When adding a signifier, QName must be a string.");}
+						if (QName.indexOf(":") < 0) {throw new Error("When adding a signifier, QName must have a registered namespace prefix or use ':' in first position to indicate default namespace.");}
+						if (QName.indexOf(":") != QName.lastIndexOf(":"))  {throw new Error("When adding a signifier, only one colon is allowed in QName string.");} 
+						if (QName.indexOf(":") == QName.length - 1)  {throw new Error("When adding a signifier, at least one additional character must follow the colon in QName string.");} 				
+						
+						if (!prefLabel) {
+							if (QName.indexOf(":") == 0)  {prefLabel = QName.substring(1);} 
+							else {prefLabel = QName.split(":")[1];}
+						}
+			
+						_QName = QName;
+						_prefLabel = prefLabel;
+					}
+				}
+			)();
+			
+			// _Axiom is an IIFE constructor function which is private to Signature
+			// _Axiom is an in-memory physical structure of triples (Nominative, Copulative, Attributative)
+			// Depending on the nature of the Copulative the same axiom might be expressed by multiple assertions, for example
+			// the axiom (triple) giraffe,rdfs:subClassOf, mammal might be asserted as
+			// giraffe isSubclassOfSuperclass mammal or
+			// mammal isSuperclassOfSubclass giraffe
+			let _Axiom = 
+			(
+				function () 
+				{
+					return function(nominative, copulative, attributative, altCopulativeLabel) 
+
+					{
+						// private to each _Axiom instance
+						let _nominative;
+						let _copulative;
+						let _attributativeSignifier;
+						let _attributativeLiteral;
+						let _copulativeLabel;
+			
+						this.getNominative = function() 
+						{
+							return _nominative;
+						};
+			
+						this.getCopulative = function() 
+						{
+							return _copulative;
+						};
+			
+						this.getCopulativeLabel = function() 
+						{
+							return _copulativeLabel;
+						};
+			
+						this.getAttributative = function() 
+						{
+							if (_attributativeSignifier) {return _attributativeSignifier;}
+							if (_attributativeLiteral) {return _attributativeLiteral;}							
+						};
+			
+						// _Axiom constructor code
+						if (_isTypeOfSignifier(nominative))
+						{
+							_nominative = nominative;
+						} 
+						if (!_nominative) 
+						{
+							var testNominative = _thisSignature.getSignifier(nominative);
+							if (testNominative) {_nominative = testNominative;}
+						}
+						if (!_nominative)
+						{
+							if (typeof nominative == 'string')
+							{
+								_nominative = _thisSignature.addSignifier(nominative);
+							}
+						}
+						if (!_nominative) {throw new Error("Invalid Nominative for new Assertion, " + nominative + ".");}
+						
+						if (_isTypeOfSignifier(copulative)) 
+						{
+							_copulative = copulative;
+						} 
+						if (!_copulative) 
+						{
+							var testCopulative = _thisSignature.getSignifier(copulative);
+							if (testCopulative) {_copulative = testCopulative;}
+						}
+						if (!_copulative)
+						{
+							if (typeof copulative == 'string')
+							{
+								_copulative = _thisSignature.addSignifier(copulative, altCopulativeLabel );
+							}
+						}
+						if (!_copulative) {throw new Error("Invalid Copulative for new Assertion, " + copulative + ".");}
+			
+						if (_isTypeOfSignifier(attributative)) 
+						{
+							_attributativeSignifier = attributative;
+						} 
+						if (!_attributativeSignifier) 
+						{
+							var testAttributative = _thisSignature.getSignifier(attributative);
+							if (testAttributative) {_attributativeSignifier = testAttributative;}
+						}
+						if (!_attributativeSignifier)
+						{
+							// if Attributative string has one colon, assume the caller wants it to be a new Signifier
+							if (typeof attributative == 'string' && attributative.indexOf(":") >= 0 && attributative.lastIndexOf(":") == attributative.indexOf(":"))
+							{
+								_attributativeSignifier = _thisSignature.addSignifier(attributative);
+							}
+						}
+						if (!_attributativeSignifier) 
+						{
+							// if Attributative string is any other string, then store it as a string literal
+							if (typeof attributative == 'string')
+							_attributativeLiteral = attributative;
+						}
+						if (!_attributativeSignifier && !_attributativeLiteral) {throw new Error("Invalid Attributative for new Assertion, " + attributative + ".");}
+			
+						_copulativeLabel = _constructCopulativeLabel(_copulative,altCopulativeLabel);
+			
+						_nominative.notifyOfParticipationAsNominative(this);
+						_copulative.notifyOfParticipationAsCopulative(this);
+						if (_isTypeOfSignifier(_attributativeSignifier)) 
+						{
+							_attributativeSignifier.notifyOfParticipationAsAttributative(this);
+						}
+					}
+			
+					function _constructCopulativeLabel(copulative,altCopulativeLabel)
+					{
+						let copulativeLabel;
+						if(altCopulativeLabel != undefined && (typeof altCopulativeLabel) == "string") 
+						{
+							copulativeLabel = altCopulativeLabel;
+						} 
+						else if(_isTypeOfSignifier(copulative))
+						{
+							CopulativeLabel = copulative.getPrefLabel();
+						}
+						return copulativeLabel;
+					}
+				}
+			)();
+
+			_Signifier.prototype = 
+			{
+				// public, non-privileged methods (one copy for all _Signifiers)
+				// uses "this" to call Attributative-specific methods, but has no access to private attributes or methods
+				log: function() 
+				{
+					console.log("Signifier = " + this.getQName());
+				}
+			};
+
+						
+			_Axiom.prototype = 
+			{
+				// public, non-privileged methods (one copy for all _Axioms)
+				// uses "this" to call Attributative-specific methods, but has no access to private attributes or methods
+				log: function() 
+				{
+					let msg  = "Nominative  " + this.getNominative().getPrefLabel() + "\nCopulative  " + this.getCopulative().getPrefLabel()  + "\nAttributative  ";
+					let testAttributative = this.getAttributative();
+					if (_isTypeOfSignifier(testAttributative))
+					{
+						msg += testAttributative.getPrefLabel();
+					}
+					else
+					{
+						msg += testAttributative.toString();
+					}
+					console.log(msg);
+				}
+			};		
+			
+			// _Signature privileged methods (defined with "this.", public, unique to each Signature instance, with access to private attributes and methods)
+			this.addNamespace = function(prefix, URI)
+			{
+        let newNamespace;
+				if (prefix.indexOf(":") >= 0) {throw new Error("When adding a namespacePrefix, a colon is not allowed in the prefix name.  Specified prefix was " + prefix);}
+				_namespaces[prefix] = URI;
+        newNamespace = prefix;
+        return newNamespace;
+			}
+
+			this.addSignifier = function(QName, prefLabel)
+			{
+				let newSignifier;
+				if (_signifiers[QName]) {
+					newSignifier = _signifiers[QName];
+				}
+				else {
+					newSignifier = new _Signifier(QName, prefLabel);
+					if (QName.indexOf(":") != 0)
+					{
+						let prefix = QName.split(":")[0];
+						if (_namespaces[prefix] == undefined) {throw new Error("When adding a signifier, QName must use an existing namespacePrefix.  Specified prefix was " + prefix);}
+					}
+		
+					_signifiers[QName] = newSignifier;	
+				}
+				return newSignifier;				
+			};
+			
+			this.getSignifier = function(signifierId)
+			{
+				let signifier = _signifiers[signifierId];
+				if (!signifier && _isTypeOfSignifier(signifierId))
+				{
+					signifier = _signifiers[signifierId.getQName()]
+				}
+				let signifierName = "";
+				if (signifier)
+				{
+					signifierName = signifier.getQName();
+				}
+				return signifier;
+			}
+
+			this.addAxiom = function(nominative, copulative, attributative, altCopulativeLabel)
+			{
+				//TODO: check if axiom already exists by checking each Nominative, Attributative, Copulative
+				let newAxiom = new _Axiom(nominative, copulative, attributative, altCopulativeLabel);
+				_axioms.push(newAxiom);
+				return newAxiom;
+			};
+
+      // TODO: getAxioms is the beginning of a query language
+
+			this.getAxiomsWithLiteralAsAttributative = function(literal) 
+			{
+				let selectedAxioms = [];
+				if (typeof literal == "string") {
+					_axioms.forEach(element => {
+						if (element.getAttributative() == literal){
+							selectedAxioms.push(element);
+						}	
+					});
+				}
+				return selectedAxioms;
+			}
+
+			// constructor code for Signature (runs once when the Attributative is instantiated with "new")
+			// ------------------------------
+    }
+	}
+)();
+
+grox.Signature.prototype = 
+{
+};
+
--- js/prj/test-model.js
+++ js/prj/test-model.js
@@ -5,103 +5,103 @@
 function handleClick()
 {   
   // TODO: reverse this, pass the schema to the model
-  let model = new grox.Model();
-  let schema = new grox.Schema(model);
+  let signature = new grox.Signature();
+//  let model = new grox.Model();
 
   // TODO: add logic in the model for each of these default namespaces
-  schema.addNamespace('grox','http://www.grox.info/');
-  schema.addSignifier('grox:hasPredicate'); // hasPredicate disallows bidirectionality. enforces asymmetry of subject and object
+  signature.addNamespace('grox','http://www.grox.info/');
+  signature.addSignifier('grox:hasPredicate'); // hasPredicate disallows bidirectionality. enforces asymmetry of subject and object
 
-  schema.addNamespace('rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns#');
-  schema.addNamespace('rdfs','http://www.w3.org/2000/01/rdf-model#');
-  schema.addNamespace('dc','http://purl.org/dc/elements/1.1/');
-  schema.addNamespace('owl','http://www.w3.org/2002/07/owl#');
-  schema.addNamespace('ex','http://www.example.org/');
-  schema.addNamespace('xsd','http://www.w3.org/2001/XMLmodel#');
-  schema.addNamespace('skos','http://www.w3.org/2004/02/skos/core#');
-  schema.addNamespace('grox','http://www.grox.info/');
-  schema.addNamespace('foaf','http://xmlns.com/foaf/0.1/');
+  signature.addNamespace('rdf','http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+  signature.addNamespace('rdfs','http://www.w3.org/2000/01/rdf-model#');
+  signature.addNamespace('dc','http://purl.org/dc/elements/1.1/');
+  signature.addNamespace('owl','http://www.w3.org/2002/07/owl#');
+  signature.addNamespace('ex','http://www.example.org/');
+  signature.addNamespace('xsd','http://www.w3.org/2001/XMLmodel#');
+  signature.addNamespace('skos','http://www.w3.org/2004/02/skos/core#');
+  signature.addNamespace('grox','http://www.grox.info/');
+  signature.addNamespace('foaf','http://xmlns.com/foaf/0.1/');
 
-  schema.addSignifier('rdf:type','isA');
-  schema.addSignifier('skos:related','isRelatedTo');
+  signature.addSignifier('rdf:type','isA');
+  signature.addSignifier('skos:related','isRelatedTo');
 
 
-  let r = schema.getSignifier('rdf:type');
+  let r = signature.getSignifier('rdf:type');
   r.log();
-  let s = schema.addSignifier(':Sam');
+  let s = signature.addSignifier(':Sam');
   s.log();
-  let w = schema.addSignifier('grox:Wally','Wallace');
+  let w = signature.addSignifier('grox:Wally','Wallace');
   w.log();
-  let t = schema.addAxiom(':Eric','rdf:type',':Father');
+  let t = signature.addAxiom(':Eric','rdf:type',':Father');
   t.log();
-  let b = schema.getSignifier(':Eric');
+  let b = signature.getSignifier(':Eric');
   if (b) {b.log()}
-  let b2 = schema.getSignifier(b);
+  let b2 = signature.getSignifier(b);
   if (b2) {b2.log()}
-  let f = schema.getSignifier(':Father');
+  let f = signature.getSignifier(':Father');
   if (f) {f.log()}
-  let z = schema.addAxiom(':Smurf','skos:related',':Munchkin'); 
+  let z = signature.addAxiom(':Smurf','skos:related',':Munchkin'); 
   z.log();
-  let j = schema.addSignifier(':Jimmy','Jimmy');
+  let j = signature.addSignifier(':Jimmy','Jimmy');
   j.log();
-  let k = schema.addAxiom(j,'rdf:type',':Father');
+  let k = signature.addAxiom(j,'rdf:type',':Father');
   k.log();
-  let l = schema.addAxiom(w,'rdf:type',f);
+  let l = signature.addAxiom(w,'rdf:type',f);
   l.log();    
-  let m = schema.addAxiom(j,':dateOfBirth','1970-07-24');
+  let m = signature.addAxiom(j,':dateOfBirth','1970-07-24');
   m.log();
-  let n = schema.getSignifier('skos:related');
+  let n = signature.getSignifier('skos:related');
   if (n) {n.log()}
 
-  let c = schema.addAxiom(':Carmen','rdf:type',':Mother');
+  let c = signature.addAxiom(':Carmen','rdf:type',':Mother');
   c.log();
 
       
   console.log('Jimmy as subject ----------------------------------------');
-  let jAxioms = j.getAxiomsWithThisAsSubject(); 
+  let jAxioms = j.getAxiomsWithThisAsNominative(); 
   jAxioms.forEach(element => {
       element.log();        
   });
 
   console.log('Father as object ----------------------------------------');
-  let fAxioms = f.getAxiomsWithThisAsObject(); 
+  let fAxioms = f.getAxiomsWithThisAsAttributative(); 
   fAxioms.forEach(element => {
       element.log();        
   });
 
   console.log('rdf:type as predicate ----------------------------------------');
-  let rAxioms = r.getAxiomsWithThisAsPredicate(); 
+  let rAxioms = r.getAxiomsWithThisAsCopulative(); 
   rAxioms.forEach(element => {
       element.log();        
   });
 
 
-  let aaaIsRed = schema.addAxiom('grox:AAA','grox:hasPredicate','red'); 
-  let aaa = schema.getSignifier('grox:AAA');
+  let aaaIsRed = signature.addAxiom('grox:AAA','grox:hasPredicate','red'); 
+  let aaa = signature.getSignifier('grox:AAA');
 
-  let bbb = schema.addSignifier('grox:BBB','bob');
-  let ccc = schema.addSignifier('grox:CCC','carmen');
-  let ddd = schema.addSignifier('grox:DDD','diego');
+  let bbb = signature.addSignifier('grox:BBB','bob');
+  let ccc = signature.addSignifier('grox:CCC','carmen');
+  let ddd = signature.addSignifier('grox:DDD','diego');
 
-  schema.addAxiom(aaa,'grox:hasPredicate','square'); 
-  schema.addAxiom(ccc,'grox:hasPredicate','red'); 
+  signature.addAxiom(aaa,'grox:hasPredicate','square'); 
+  signature.addAxiom(ccc,'grox:hasPredicate','red'); 
 
   console.log('AAA as subject -------------------------------------');
 
-  (aaa.getAxiomsWithThisAsSubject()).forEach( element => {
+  (aaa.getAxiomsWithThisAsNominative()).forEach( element => {
       element.log();
       }
   )
 
   console.log('red as predicate ----------------------------------------');
-  let redTheorems = schema.getAxiomsWithLiteralAsObject('red'); 
-  redTheorems.forEach(element => {
+  let redAxioms = signature.getAxiomsWithLiteralAsAttributative('red'); 
+  redAxioms.forEach(element => {
       element.log();        
   });
 
   console.log('red as predicate, AAA as subject with alice prefLabel ---------------------------');
   aaa.setPrefLabel('alice');
-  redTheorems.forEach(element => {
+  redAxioms.forEach(element => {
       element.log();        
   });
 
